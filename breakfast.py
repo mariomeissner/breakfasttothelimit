f72cccc98fc6eee94aa2248e37422fdd82ac65cc
Mario Meissner <mr.rash.boy@gmail.com>
2017-11-13 14:19:07 +0100

    changes

--- breakfast.py
+++ breakfast.py
@@ -12,13 +12,24 @@ class Manager:
     def __init__(self):
         """ Set up the system by asking the user for the initializing information """
         self.num_users = int(input("How many users will use the system?: "))
-        self.names = ["" for i in range(self.num_users)]
-        self.work_locations = ["" for i in range(self.num_users)]
         self.wg_location = input("Where do the users live? :")
+        self.roommates = []
+        self.lamps = []
 
         for i in range(self.num_users):
-            self.names[i] = input("What's the name of user %d?: " % (i))
-            self.work_locations[i] = input("Where does %s go to work?: " % self.names[i])
+
+            name = input("What's the name of user %d?: " % (i))
+            working_location = input("Where does %s go to work?: " % self.names[i])
+            start_time = input("When does %s start working?: ")
+            self.roommates.append(Roommate(name, working_location, start_time))
+
+        print("System is ready to go")
+
+
+    def remaining_time()
+        """Connect to the API and request the time needed to to go work"""
+
+    
 
 class Roommate:
 
