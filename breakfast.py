d02644398e26e8c57671616ccfb2ef7198d7141f
Mario Meissner <mr.rash.boy@gmail.com>
2017-11-13 14:09:04 +0100

    tyhings

--- breakfast.py
+++ breakfast.py
@@ -7,14 +7,19 @@ Authors: M. Meissner and M. Mueller
 
 """
 class Manager:
+    """ Global manager that queries the API's and updates lights according to remaining time"""
 
-    def __init__(object):
+    def __init__(self):
         """ Set up the system by asking the user for the initializing information """
-        num_users = int(input("How many users will use the system?: "))
-        names = ["" for i in range(num_users)]
-        work_locations = ["" for i in range(num_users)]
-        wg_location = input("Where do the users live? :")
+        self.num_users = int(input("How many users will use the system?: "))
+        self.names = ["" for i in range(self.num_users)]
+        self.work_locations = ["" for i in range(self.num_users)]
+        self.wg_location = input("Where do the users live? :")
 
-        for i in range(num_users):
-            names[i] = input("What's the name of user %d?: " % (i))
-            work_locations[i] = input("Where does %s go to work?: " % names[i])
+        for i in range(self.num_users):
+            self.names[i] = input("What's the name of user %d?: " % (i))
+            self.work_locations[i] = input("Where does %s go to work?: " % self.names[i])
+
+class Roommate:
+
+class Lamp:
