359bd04ea6fe609da2038509df2876422e58e79f
Melanie <muell162@hm.edu>
2017-11-25 17:10:15 +0100

    added lamp test

---
+++ test_lamps.py
@@ -0,0 +1,9 @@
+#http://localhost/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/groups/0/state
+
+from breakfast import Lamp
+
+lamp1 = Lamp()
+lamp2 = Lamp()
+lamp3 = Lamp()
+
+lamp1.set_white(0)
