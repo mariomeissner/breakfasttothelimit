0d76993aa743ea77c7162c06db20092686dfb958
Melanie <muell162@hm.edu>
2017-11-26 22:16:22 +0100

    .

--- test_lamps.py
+++ test_lamps.py
@@ -1,9 +1,11 @@
+
+# -*- coding: utf-8 -*-
 #http://localhost/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/groups/0/state
 
 from breakfast import Lamp
 
-lamp1 = Lamp()
-lamp2 = Lamp()
-lamp3 = Lamp()
+lamp0 = Lamp(0)
+lamp1 = Lamp(1)
+lamp2 = Lamp(2)
 
-lamp1.set_white(0)
+lamp1.set_white()
