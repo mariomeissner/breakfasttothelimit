04b27e350d14bf1bf420e248975b5cedfa2310b3
Melanie <muell162@hm.edu>
2017-11-29 19:36:13 +0100

    chnges

--- breakfast.py
+++ breakfast.py
@@ -3,6 +3,7 @@ from datetime import datetime, date, time
 import json
 import googlemaps
 import requests
+from time import sleep
 
 """ Optimize your breakfast!
 
@@ -25,11 +26,14 @@ class Manager:
 
             name = input("What's the name of user %d?: " % (i))
             working_location = input("Where does %s go to work?: " % name)
-            start_time = input("When does %s start working?: ")
+            start_time_hour = int(input("When does %s start working (hour)?: "))*3600
+            start_time_minute = int(input("When does %s start working (minute)?: "))*60
+            self.start_time = start_time_hour + start_time_minute
+
             transport = input("")
             lamp = Lamp(i+1)
             self.roommates.append(Roommate(name, working_location, 
-                start_time, transport, lamp))
+                self.start_time, transport, lamp))
 
         self.gmaps = GMapsClient(self.wg_location)
         
@@ -40,8 +44,8 @@ class Manager:
         """Connect to the API and request the time needed to to go work"""
         d = datetime.now()
         time_Seconds = (d.hour*3600 + d.minute*60 + d.second)
-        travel_Time = self.gmaps.travel_time(r[0][1])
-        difference = time_Seconds - travel_Time
+        travel_Time = self.gmaps.travel_time(self.wg_location)
+        difference =  self.start_time - (time_Seconds + int(travel_Time))
         return difference
 
        
@@ -61,26 +65,35 @@ class Lamp:
     """ Stores Lamp's data e.g. color"""  
     #TODO: Use this to send HTTP to Lamps
     #http://docs.python-requests.org/en/master/user/quickstart/
-    white = {"on":"true", "bri":255, "sat":255, "hue":6500, "ct":153}
-    orange = {"on":"true", "bri":255, "sat":255, "hue":9000}
-    red = {"on":"true", "bri":255, "sat":255, "hue":0}
+    w = {"on":True, "bri":255, "sat":255, "hue":6500, "ct":153}
+    white = json.dumps(w)
+    o = {"on":True, "bri":255, "sat":255, "hue":9000}
+    orange = json.dumps(o)
+    r = {"on":True, "bri":255, "sat":255, "hue":0}
+    red = json.dumps(r)
+    on = json.dumps({"on":True})
+    off = json.dumps({"on":False})
 
     def __init__(self,i):
-        requests.put("http://10.28.209.13:9003/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/groups/0/state", data=self.white)
-        #requests.put("http://localhost:80/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/groups/0/state", data=self.white)
         self.lampID = i
+        response = requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/' + str(self.lampID) + '/state', data=self.white)
+        #print(response.text)
 
     def set_white(self):
-        requests.put("http://10.28.209.13:9003/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/" + str(self.lampID) + "/state", data=self.white)
-        #requests.put("http://localhost:80/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/" + str(self.lampID) + "/state", data=self.white)
+        requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/' + str(self.lampID) + '/state', data=self.white)
 
     def set_orange(self):
-        requests.put("http://10.28.209.13:9003/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/" + str(self.lampID) + "/state", data=self.orange)
-        #requests.put("http://localhost:80/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/" + str(self.lampID) + "/state", data=self.orange)
-        
+        requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/' + str(self.lampID) + '/state', data=self.orange)
+ 
     def set_red(self):
-        requests.put("http://10.28.209.13:9003/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/" + str(self.lampID) + "/state", data=self.red)
-        #requests.put("http://localhost:80/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/" + str(self.lampID) + "/state", data=self.red)
+        requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/' + str(self.lampID) + '/state', data=self.red)
+
+    def turn_on(self):
+        requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/' + str(self.lampID) + '/state', data=self.red)  
+
+    def turn_off(self):
+        requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/' + str(self.lampID) + '/state', data=self.red)      
+
 
 class GMapsClient:
 
@@ -95,17 +108,33 @@ class GMapsClient:
         #https://developers.google.com/maps/documentation/distance-matrix/start?hl=de
         directions_result = self.client.directions(self.wg_location, destination, mode="transit", departure_time=now)
         # output_format='json', transit_mode=transitmode ??
-        duration = (directions_result[0]['legs'][0]['duration']['value'])
+        duration = int((directions_result[0]['legs'][0]['duration']['value']))
         #json stores information in dictionaries
         #TODO: test if extracting and returning works
         return duration
 
 
 if __name__ == '__main__':
-    #manager = Manager()
-    gmaps = GMapsClient("Olympiazentrum, 80809 München")
-    print(gmaps.travel_time("Lothstraße 64, 80335 München"))
-    l1 = Lamp(0)
-    l1.set_red()
+    manager = Manager()
+    print("about to enter while true loop")
+    while (True):
+        print("start sleeping")
+        sleep(5)
+        print("Finished sleeping")
+        for i in range(len(manager.roommates)):
+            if int(manager.remaining_time(i)) <= 0:
+                print("drin")
+                while(True):
+                    manager.roommates[i].lamp.turn_off()
+                    sleep(2)
+                    manager.roommates[i].lamp.turn_on()
+                    sleep(2)
+                    break
+            elif 0 <= int(manager.remaining_time(i)) <= 60:  
+                manager.roommates[i].lamp.set_red()
+            elif 60 <= int(manager.remaining_time(i)) <= 180:
+                manager.roommates[i].lamp.set_orange()      
+
+    
 
     
--- test_lamps.py
+++ test_lamps.py
@@ -3,9 +3,14 @@
 #http://localhost/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/groups/0/state
 import requests
 from breakfast import Lamp
-data = {"on":'true'}
+import json
 
-requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/1/state', data)
-requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/2/state', data)
-requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/3/state', data)
 
+parsed =json.dumps({'on':False})
+print(parsed)
+
+response = requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/1/state', parsed)
+print(response.text)
+requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/2/state', parsed)
+requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/3/state', parsed)
+print(data)
