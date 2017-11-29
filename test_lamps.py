17b7ba4b30d977fa800de13a28e1a7e4e4764ebf
Melanie <muell162@hm.edu>
2017-11-29 17:45:28 +0100

    changed request

--- test_lamps.py
+++ test_lamps.py
@@ -1,11 +1,11 @@
 
 # -*- coding: utf-8 -*-
 #http://localhost/debug/clip.html/api/2b2d3ff23d63751f10c1d8c0332d50ff/groups/0/state
-
+import requests
 from breakfast import Lamp
+data = {"on":'true'}
 
-lamp0 = Lamp(0)
-lamp1 = Lamp(1)
-lamp2 = Lamp(2)
+requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/1/state', data)
+requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/2/state', data)
+requests.put('http://10.28.209.13:9003/api/2b2d3ff23d63751f10c1d8c0332d50ff/lights/3/state', data)
 
-lamp1.set_white()
